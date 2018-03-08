459db995464102b3249baef798b6662451e9ecec
TLESORT <t.lesort@gmail.com>
2018-03-08 16:29:00 +0100

    fix of another bug

--- data/raw/raw.py
+++ data/raw/raw.py
@@ -13,9 +13,11 @@ import os
 cifar_path = "cifar-100-python.tar.gz"
 mnist_path = "mnist.npz"
 
+print(os.path.exists(cifar_path))
+
 if not os.path.exists(cifar_path):
     # URL from: https://www.cs.toronto.edu/~kriz/cifar.html
-    subprocess.call(cifar_path, shell=True)
+    subprocess.call("wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz", shell=True)
 
 subprocess.call("tar xzfv cifar-100-python.tar.gz", shell=True)
 
